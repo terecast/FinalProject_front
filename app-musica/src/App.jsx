6700ab077e8a09b75af479b45b36328b402d94a6
tere <125310682+terecast@users.noreply.github.com>
2023-12-14 14:52:41 -0600

    Avance Front

--- app-musica/src/App.jsx
+++ app-musica/src/App.jsx
@@ -1,12 +1,17 @@
-
+import { Sidebar } from "./components/navigation/Sidebar";
+import { Dashboard } from "./components/views/Dashboard";
 
 function App() {
- 
   return (
     <>
-      <h1>Hola Mundo</h1>
+      <main class="relative h-screen overflow-hidden bg-gray-100 dark:bg-gray-800">
+        <div class="flex items-start justify-between">
+          <Sidebar />
+          <Dashboard />
+        </div>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
